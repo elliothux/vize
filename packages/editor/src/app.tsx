3798e2b1ad731602a9cc9e72c38f0e402d973fe4
HuQingyang <i@do.codes>
2019-09-17 01:10:18 +0800

    feat: adding editor folder

---
+++ packages/editor/src/app.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { render} from 'react-dom';
+import {Entry} from "./components/Entry";
+
+
+render(<Entry/>, document.querySelector('#root'));
---
+++ packages/editor/src/components/Entry/index.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export function Entry() {
+    return <div>1</div>
+}
