838471aefb074ec5285438dcc8b0317bfbedc7f0
qingyang.huqingyan <qingyang.huqingyan@alibaba-inc.com>
2020-08-09 14:37:02 +0800

    chore: add some eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,25 @@ module.exports = {
         'plugin:prettier/recommended',
     ],
     rules: {
+        'max-lines-per-function': [
+            'error',
+            {
+                max: 60,
+                skipBlankLines: true,
+                skipComments: true,
+            },
+        ],
+        'max-lines': [
+            'error',
+            {
+                max: 120,
+                skipBlankLines: true,
+                skipComments: true,
+            },
+        ],
+        'max-nested-callbacks': ['error', { max: 4 }],
+        'max-params': ['error', { max: 5 }],
+        'max-depth': ['error', { max: 5 }],
         '@typescript-eslint/ban-types': 0,
         '@typescript-eslint/explicit-module-boundary-types': 0,
         '@typescript-eslint/no-non-null-assertion': 0,
