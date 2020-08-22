be73804278828871768d4ba7182522633e1db9be
Copyes <fan_545697167@qq.com>
2020-08-22 12:02:45 +0800

    fix: bug fixed

--- packages/materials-universal/src/components/Button/index.tsx
+++ packages/materials-universal/src/components/Button/index.tsx
@@ -2,11 +2,16 @@ import * as React from 'react';
 import './index.scss';
 
 interface Props {
-    data: {
-        text: string;
-    };
+  data: {
+    text: string;
+  };
+  commonStyle: object;
 }
 
-export default function Button({ data: { text } }: Props) {
-    return <button className="vize-materials-universal button">{text}</button>;
+export default function Button({ data: { text }, commonStyle }: Props) {
+  return (
+    <button className="vize-materials-universal button" style={commonStyle}>
+      {text}
+    </button>
+  );
 }
