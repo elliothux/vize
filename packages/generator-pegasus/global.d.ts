94493d23a256b80c05f6a0bdcdd4143b5db5220c
qingyang.huqingyan <qingyang.huqingyan@alibaba-inc.com>
2021-02-01 22:55:30 +0800

    feat(management-ui & cgi): generator

--- packages/generator-pegasus/global.d.ts
+++
@@ -1,3 +0,0 @@
-declare module 'mini-css-extract-plugin';
-
-type Maybe<T> = T | null | undefined;
--- packages/generator-pegasus/src/types/index.ts
+++
@@ -1,3 +0,0 @@
-export * from '@vize/types/src';
-export * from './paths';
-export * from './template';
