3e5274c36c29ec551103fb23557edc01d2c48897
Aedron <534559077@qq.com>
2021-05-13 00:32:39 +0800

    feat: new material universal component: Video

---
+++ packages/materials-universal/src/components/Video/config.ts
@@ -0,0 +1,41 @@
+const info = {
+  name: '视频',
+  desc: '基础视频组件',
+  author: 'Aedron@github',
+};
+
+const dataForm = {
+  url: {
+    title: '视频URL',
+    type: 'string',
+    default: '',
+    required: true,
+  },
+  poster: {
+    title: '封面图',
+    type: 'string',
+    widget: 'image',
+  },
+  autoPlay: {
+    title: '自动播放',
+    description: '自动播放时视频默认静音',
+    type: 'boolean',
+    default: false,
+  },
+};
+
+const enableStyleGroup = {
+  size: true,
+  transform: true,
+  border: true,
+  margin: true,
+  padding: true,
+  zIndex: true,
+  position: true,
+};
+
+export default {
+  info,
+  dataForm,
+  enableStyleGroup,
+};
