1169b30636a62e8c89c1c642208f59bd83b4b7b6
Aedron <534559077@qq.com>
2021-05-22 02:26:43 +0800

    feat(materials-universal): new component "Alert"

---
+++ packages/materials-universal/src/components/Alert/config.ts
@@ -0,0 +1,51 @@
+const info = {
+  name: '弹窗提示',
+  desc: '通用弹窗提示组件（Alert）',
+  author: 'Aedron@github',
+};
+
+const dataForm = {
+  title: {
+    title: '标题',
+    type: 'string',
+    default: '弹窗标题',
+    required: true,
+  },
+  desc: {
+    title: '描述内容',
+    type: 'string',
+    widget: 'textarea',
+    default: '',
+  },
+  okText: {
+    title: '确认按钮文案',
+    type: 'string',
+    default: '确认',
+    required: true,
+  },
+  cancelText: {
+    title: '取消按钮文案',
+    type: 'string',
+    default: '取消',
+  },
+  jumpUrl: {
+    title: '确认跳转链接',
+    type: 'string',
+    format: 'url',
+  },
+};
+
+export default {
+  info,
+  dataForm,
+  onEvents: [
+    { displayName: '切换弹窗', eventName: 'toggle' },
+    { displayName: '打开弹窗', eventName: 'open' },
+    { displayName: '关闭弹窗', eventName: 'close' },
+  ],
+  emitEvents: [
+    { displayName: '打开弹窗', eventName: 'open' },
+    { displayName: '关闭弹窗', eventName: 'close' },
+  ],
+  hideEditMask: true,
+};
