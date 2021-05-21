5444e2cd1e386cefd8492398668d6d1552013726
Aedron <534559077@qq.com>
2021-05-22 02:27:03 +0800

    feat(materials-universal): new component "Container"

---
+++ packages/materials-universal/src/components/Container/config.ts
@@ -0,0 +1,27 @@
+const info = {
+  name: '通用容器',
+  desc: '通用容器组件',
+  author: 'Aedron@github',
+};
+
+const enableStyleGroup = {
+  transform: true,
+  border: true,
+  background: true,
+  margin: {
+    top: 12,
+    bottom: 12,
+  },
+  padding: {
+    left: 8,
+    right: 8,
+  },
+  zIndex: true,
+  position: true,
+};
+
+export default {
+  info,
+  enableStyleGroup,
+  isContainer: true,
+};
