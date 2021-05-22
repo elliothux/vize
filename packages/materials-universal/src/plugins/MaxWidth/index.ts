bdf41e01afe1b15984dc790a0c327de4dcd7a207
Aedron <534559077@qq.com>
2021-05-22 23:01:07 +0800

    feat(materials-universal): new plugin "MaxWidth"

---
+++ packages/materials-universal/src/plugins/MaxWidth/index.ts
@@ -0,0 +1,17 @@
+import { ComponentProps } from '@vize/types';
+
+interface Data {
+  maxWidth: number;
+}
+
+export function maxWidth({ data: { maxWidth } }: ComponentProps<Data>) {
+  const style = document.createElement('style');
+  style.innerHTML = `\n
+  body {
+    max-width: ${maxWidth}px !important;
+    margin-left: auto !important;
+    margin-right: auto !important;
+  }`;
+  style.className = 'vize-materials-universal-plugin-max-width';
+  document.head.appendChild(style);
+}
