4f5dbb8255ce4f0cca22cf6eae48bd5670ab1948
Aedron <534559077@qq.com>
2021-06-07 01:00:05 +0800

    feat(management-ui): support search & refresh logs

--- packages/management-ui/src/pages/logs/LogItem.tsx
+++ packages/management-ui/src/pages/logs/LogItem.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import { Popover } from 'antd';
+import { BiInfoCircle } from 'react-icons/bi';
 import { LogItem as Item } from './types';
 
 interface Props {
@@ -22,10 +24,21 @@ interface ItemProps {
 function LogItem({ item }: ItemProps) {
   return (
     <div className="log-content-item">
+      <Popover
+        title={JSON.stringify(item, null, 4)
+          .split('\n')
+          .map((i, index) => (
+            <p key={index}>{i}</p>
+          ))}
+      >
+        <BiInfoCircle />
+      </Popover>
+
       <div className="log-item-meta">
         <span className="timestamp">{item.timestamp}</span>
         <span className={`level ${item.level}`}>{item.level}</span>
       </div>
+
       <div>
         <span className="context">[{item.context}]</span>
         <span className="message">{item.message}</span>
